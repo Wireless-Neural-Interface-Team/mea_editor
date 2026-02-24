eff2584208970b09df253b330306146d59888229
hepierrey <pierre.he@inserm.fr>
2026-02-24 15:35:56 +0100

    Run_mea_editor.py update
    Update for Pyinstaller that only works with absolute path

--- mea_editor/run_mea_editor.py
+++ mea_editor/run_mea_editor.py
@@ -1,7 +1,7 @@
 """
 Launcher script for PyInstaller.
 """
-from .electrode_array_editor_qt import run_app
+from mea_editor.electrode_array_editor_qt import run_app
 
 if __name__ == "__main__":
     run_app()
